942bd048eae1a3eaac7a4f21b7e58e86fb395c77
Oreal51 <Oreal51@Oreal51deMacBook-Pro.local>
2017-06-12 07:13:23 +0800

    Initial Commit

---
+++ Shadow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shadow
+//
+//  Created by Oreal51 on 2017/6/12.
+//  Copyright © 2017年 Oreal51. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Shadow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Shadow
+//
+//  Created by Oreal51 on 2017/6/12.
+//  Copyright © 2017年 Oreal51. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
